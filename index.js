15a1125ad0e6bc4866224a017d630a9a5b2d9579
hilwa173 <mhikma385@gmail.com>
2024-06-08 09:38:37 +0300

    js linter fixed

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
 function openham() {
-    const navigation1 = document.querySelector('.navigation1');
-    navigation1.style.display = 'flex';
-                }
- function closeham() {
-   const navigation1 = document.querySelector('.navigation1');
- navigation1.style.display = 'none';
-                }
-        // Show the login form container when the login button is clicked
+  const navigation1 = document.querySelector('.navigation1');
+  navigation1.style.display = 'flex';
+}
+const button = document.querySelector('.nav-menu-btn');
+button.addEventListener('click', openham);
+
+function closeham() {
+  const navigation1 = document.querySelector('.navigation1');
+  navigation1.style.display = 'none';
+}
+// Show the login form container when the login button is clicked
 document.querySelector('#login-btn').onclick = () => {
-    document.querySelector('.login-form-container','.signup-form').classList.add('active');
-    
-}
+  document.querySelector('.login-form-container', '.signup-form').classList.add('active');
+  closeham();
+};
 
-function showSign(){
-const news = document.querySelector('.signup-form');
-news.style.display = 'flex';
+document.getElementById('sign-btn').onclick = function showSign() {
+  const news = document.querySelector('.signup-form');
+  news.style.display = 'flex';
 
-const lg = document.querySelector('.login-form-container');
-lg.style.display = 'none';
-}
+  const lg = document.querySelector('.login-form-container');
+  lg.style.display = 'none';
+};
 
-function showLogin(){
-const logs = document.querySelector('.login-form-container');
-logs.style.display = 'flex';
+document.getElementById('lgn-btn').onclick = function showLogin() {
+  const logs = document.querySelector('.login-form-container');
+  logs.style.display = 'flex';
 
-const hideLog = document.querySelector('.signup-form');
-hideLog.style.display = 'none';
-}
+  const hideLog = document.querySelector('.signup-form');
+  hideLog.style.display = 'none';
+};
 
+document.getElementById('close-login-form').onclick = function closeLoginForm() {
+  document.querySelector('#close-login-form').onclick = () => {
+    document.querySelector('.login-form-container').classList.remove('active');
+  };
+};
 
-function closeLoginForm() {
-    document.querySelector('#close-login-form').onclick = () => {
-        document.querySelector('.login-form-container').classList.remove('active');
-    }
-      }    
-  
-    function closeSignForm() {
-       const sgn= document.querySelector('.signup-form');
-       sgn.style.display='none';
-        }
-
-        function hide(){
-            const trys=document.querySelector('logintry');
-            trys.style.display='none';
-        }
+document.getElementById('close-sign-form').onclick = function closeSignForm() {
+  const sgn = document.querySelector('.signup-form');
+  sgn.style.display = 'none';
+};
